b2f90c032bf0955bec44624584308b8d2d7853a0
Akanksha Trehun <akankshatrehun@gmail.com>
2026-02-28 10:03:09 +0530

    feat: Add analytics dashboard service

---
+++ js/analytics/AnalyticsService.js
@@ -0,0 +1,19 @@
+// Analytics Service
+class AnalyticsService {
+    trackStudyTime(topic, duration) {
+        // Track study time
+    }
+    
+    getProgress() {
+        return {
+            totalTime: 0,
+            topicsCompleted: 0,
+            streak: 0
+        };
+    }
+    
+    getHeatmapData() {
+        return [];
+    }
+}
+
